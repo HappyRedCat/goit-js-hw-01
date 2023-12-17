eed1acfdebf97a012da033b63ed3206e7512a393
HappyRedCat <HappyRedCat@users.noreply.github.com>
2023-12-17 20:44:55 +0100

    update

---
+++ js/01-vars.js
@@ -0,0 +1,54 @@
+/**
+ * Змінні та типи даних
+ *
+ * - Оголошення змінних з let и const
+ * - Перевизначення значення
+ * - Найменування змінних
+ * - Вибір між const і let
+ * - Числа, рядки, булі, null, undefined
+ * - Оператор typeof
+ */
+// const x = 10; // number
+// const y = 'Hello World'; //string
+// const q = false; // true or false // boolean
+// const t = null;
+// const r = undefined;
+
+// =======================
+// let userName1 = 10;
+// let userName2 = 20;
+
+// console.log(userName2);
+
+// let userName;
+// userName = 'Hello World';
+
+// const PI = 3.1415926;
+// PI = "Hello World";
+
+// =======================
+
+// const city = "Dnipro";
+// console.log(city);
+
+// =======================
+
+// let userName = "Hello";
+
+// console.log(typeof 10);
+// console.log(typeof "Hello World");
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// const x = 10;
+
+// const result = typeof x;
+
+// console.log(typeof result);
+
+// console.log(10);
+// console.log("10");
+
+// console.log(true);
+// console.log("true");
---
+++ js/03-operators.js
@@ -0,0 +1,67 @@
+/**
+ * Арифметичні операції
+ * - Оператори +, -, *, /, %, **
+ * - Комбіновані оператори
+ */
+
+// const a = 10;
+// const b = 5;
+// const result1 = (a + b + b) * 2;
+// const result2 = a - b;
+// const result3 = a * b;
+// const result4 = a / b;
+// const result5 = a % b;
+// const result6 = 2 ** 4;
+// console.log(10 / 0);
+// console.log(result6);
+
+// ========
+
+/* 
+
+10 % 7 = 10 - (7*1) = 3;
+55 % 3 = 55 - (3 * 18) = 1;
+56 % 3 = 56 - (3 * 18) = 2;
+57 % 3 = 57 - (3 * 19) = 0;
+
+21 % 7 = 0;
+22 % 7 = 1; 
+*/
+
+// let x = 15;
+
+// x = x + 15;
+// x += 15;
+
+// let y = 30;
+// y -= 5;
+
+// let q = 5;
+// q -= 2;
+// console.log(q);
+
+/**
+ * Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+ */
+
+const apples = 47;
+const grapes = 135;
+
+// console.log("Sum:", apples + grapes);
+// console.log("Diff:", grapes - apples);
+
+/**
+ * Заміни вираз перевизначення комбінованим оператором
+ */
+
+// let students = 100;
+// students = students + 50;
+// students += 50;
+// console.log(students);
+
+/**
+ * Розбери пріоритет операторів в інструкції привласнення значення змінної result
+ */
+
+// const result = (108 + 223 - 2) * 5;
+// console.log(result);
